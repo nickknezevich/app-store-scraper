4685873b57bcd54f99e20347d23e90a42b5f09b8
Nikola Knezevic <48396463+nickknezevich@users.noreply.github.com>
2023-08-27 12:51:47 -0700

    updates

--- server/src/reports/filters/widget.filter.ts
+++
@@ -1,8 +0,0 @@
-export type WidgetFilter = {
-    product_id: number;
-    customer_id: number;
-    supplier_id: number;
-    warehouse_id: number;
-    page: number;
-    num_per_page: number;
-}
--- server/src/reports/responses/report.response.ts
+++
@@ -1,8 +0,0 @@
-interface ReportResponse {
-    report_data: unknown[];
-    pagination: {
-        total: number;
-        page: number;
-        total_pages: number;
-    }
-}
